38072351419773c3a06aa174af05be0f1a894d9f
mkL <maiquelbald@gmail.com>
2022-06-11 15:37:09 -0300

    added discord.js

---
+++ bot/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', async () => {
+    console.log("Bot ready!");
+});
